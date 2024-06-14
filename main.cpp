b2de907bd3f672b1d303b0ee0f6c03f888376235
CAMF0712 <126723782+CAMF0712@users.noreply.github.com>
2024-06-14 09:50:05 -0600

    arreglo de errores

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
+#include "client.hpp"
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void createTestFiles() {
+    std::ofstream("file1.txt") << "This is file 1" << std::endl;
+    std::ofstream("file2.txt") << "This is file 2" << std::endl;
+    std::ofstream("file3.txt") << "This is file 3" << std::endl;
+}
+
+void printUsage() {
+    std::cout << "Usage:" << std::endl;
+    std::cout << "  guit init <name>" << std::endl;
+    std::cout << "  guit add [-A] [name]" << std::endl;
+    std::cout << "  guit commit <message>" << std::endl;
+    std::cout << "  guit sync <file>" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    createTestFiles();
+
+    if (argc < 2) {
+        printUsage();
+        return 1;
+    }
+
+    std::string command = argv[1];
+    if (command == "init" && argc == 3) {
+        Client client(argv[2]);
+        client.init();
+    } else if (command == "add") {
+        bool add_all = false;
+        std::vector<std::string> files;
+        for (int i = 2; i < argc; ++i) {
+            std::string arg = argv[i];
+            if (arg == "-A") {
+                add_all = true;
+            } else {
+                files.push_back(arg);
+            }
+        }
+        Client client("my_repo");
+        client.add(files, add_all);
+    } else if (command == "commit" && argc == 3) {
+        Client client("my_repo");
+        client.commit(argv[2]);
+    } else if (command == "sync" && argc == 3) {
+        Client client("my_repo");
+        client.sync(argv[2]);
+    } else {
+        printUsage();
+    }
+
     return 0;
 }
--- utils.cpp
+++ utils.cpp
@@ -1,15 +1,38 @@
 #include "utils.hpp"
-#include <openssl/md5.h>
+#include <openssl/evp.h>
 #include <iomanip>
 #include <sstream>
 #include <filesystem>
+#include <stdexcept> // Añadir para la excepción std::runtime_error
 
 std::string computeMD5(const std::string& data) {
-    unsigned char result[MD5_DIGEST_LENGTH];
-    MD5((unsigned char*)data.c_str(), data.size(), result);
+    unsigned char result[EVP_MAX_MD_SIZE];
+    unsigned int result_len;
+
+    EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
+    if (mdctx == nullptr) {
+        throw std::runtime_error("Failed to create EVP_MD_CTX");
+    }
+
+    if (1 != EVP_DigestInit_ex(mdctx, EVP_md5(), nullptr)) {
+        EVP_MD_CTX_free(mdctx);
+        throw std::runtime_error("Failed to initialize MD5 digest");
+    }
+
+    if (1 != EVP_DigestUpdate(mdctx, data.c_str(), data.size())) {
+        EVP_MD_CTX_free(mdctx);
+        throw std::runtime_error("Failed to update MD5 digest");
+    }
+
+    if (1 != EVP_DigestFinal_ex(mdctx, result, &result_len)) {
+        EVP_MD_CTX_free(mdctx);
+        throw std::runtime_error("Failed to finalize MD5 digest");
+    }
+
+    EVP_MD_CTX_free(mdctx);
 
     std::ostringstream oss;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
+    for (unsigned int i = 0; i < result_len; ++i) {
         oss << std::hex << std::setw(2) << std::setfill('0') << (int)result[i];
     }
     return oss.str();
